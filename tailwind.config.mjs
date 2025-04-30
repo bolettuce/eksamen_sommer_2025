81b04c881c30972077881fabfde55da0895c76b3
bolettuce <bobi0001@stud.kea.dk>
2025-04-30 23:07:20 +0200

    Tilføjelse af landing og tekst animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -81,11 +81,14 @@ export default {
                   '0%': { opacity: '0', transform: 'translateY(20px)' },
                   '100%': { opacity: '1', transform: 'translateY(0)' },
                 },
+
               },
               animation: {
                 'spin-once': 'spin360 1s ease-in-out',
                 'fade-up': 'fade-up 0.5s ease-out forwards',
               },
+
+              
               
             spacing: {
                 '2xs': '2px',
