f89968f94784390aaabe3c2b1404932109ea85e8
bolettuce <bobi0001@stud.kea.dk>
2025-12-15 22:41:34 +0100

    Use static artworks data and new bucket URLs

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pfteqqpsagfcztycwohv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmdGVxcXBzYWdmY3p0eWN3b2h2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NjYyNzAyNywiZXhwIjoyMDYyMjAzMDI3fQ.7zAZH-bTmnV6ZPrBWIXHEXn0_MCdEhNuTsBleVM4KI8'
+const supabaseUrl = 'https://izzrgdiwpkuoqjpmqgtu.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6enJnZGl3cGt1b3FqcG1xZ3R1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NzE0MTUsImV4cCI6MjA3NzE0NzQxNX0.IvVyEBFtHEXaDpqwYdwZbb3DzKR4_2qcmEsHTRSyS_k'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
